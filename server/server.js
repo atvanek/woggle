43597b8e44b1416e037eea45c068de518aba5baa
atvanek <99850013+atvanek@users.noreply.github.com>
2023-04-03 20:09:05 -0400

    Webpack config

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/api', (req, res) => {
+	console.log('request received AGAIN');
+	res.json('this is the API');
+});
+
+app.use('/other', (req, res) => {
+	console.log('request received AGAIN');
+	res.json('this is the OTHER');
+});
+
+app.use('/testWord', (req, res) => {
+	console.log(req.body);
+	const { word } = req.body;
+	fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data);
+			if (!Array.isArray(data)) {
+				console.log('invalid');
+				res.json({ word: 'invalid' });
+			} else {
+				res.json({ word: 'valid' });
+			}
+		});
+});
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}...`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //pulls css out of bundle.js
+
+module.exports = {
+	entry: './client/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'), // go back to this, is the right path?
+		filename: 'bundle.js',
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './index.html', //why do we need a template but in the webpack.js.org website, it doesn't use  a template?? IS IT BECAUSE THEY STILL HAVE THEIR BUNDLE JS FILE
+		}),
+		new MiniCssExtractPlugin(), //adds miniCSS to plugins
+	],
+	mode: process.env.NODE_ENV,
+	module: {
+		rules: [
+			{
+				test: /\.jsx?/, //? means last char is optional
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
+				},
+			},
+			{
+				test: /\.s?css/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'style-loader',
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, '/client'),
+		},
+		compress: true,
+		port: 8080,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+			},
+		},
+	},
+};
