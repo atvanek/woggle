31927133b9d47924fcef29525d675c247284a956
atvanek <99850013+atvanek@users.noreply.github.com>
2023-05-30 16:22:07 -0400

    Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
 	root: 'src',
 	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 	server: {
+		host: 'localhost',
 		port: 8080,
 		proxy: {
 			'/api': {
