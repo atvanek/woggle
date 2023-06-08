1445b5abcd90681da8b5902ad43d6327b6424487
atvanek <99850013+atvanek@users.noreply.github.com>
2023-06-08 15:22:55 -0400

    Rotate cube ani with border

--- src/client/components/Box.jsx
+++ src/client/components/Box.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 function Box({ letter, id, handleClick, className }) {
 	return (
-		<div
-			id={id}
-			onClick={handleClick}
-			className={`${className} box flex center-all pointer`}>
-			{letter}
+		// <></>
+		// <div
+		// 	id={id}
+		// 	onClick={handleClick}
+		// 	className={`${className} box flex center-all pointer`}>
+		// 	{letter}
+		// </div>
+
+		<div class='scene'>
+			<div class='cube pointer' onClick={handleClick}>
+				<div class='cube__face cube__face--front flex center-all'>{letter}</div>
+				<div class='cube__face cube__face--back flex center-all'>{letter}</div>
+				<div class='cube__face cube__face--right flex center-all'>{letter}</div>
+				<div class='cube__face cube__face--left flex center-all'>{letter}</div>
+				<div class='cube__face cube__face--top flex center-all'></div>
+				<div class='cube__face cube__face--bottom flex center-all'></div>
+			</div>
 		</div>
 	);
 }
