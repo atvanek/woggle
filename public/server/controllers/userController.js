189e5d297200f2e4b310d826707429a37a0a3455
atvanek <99850013+atvanek@users.noreply.github.com>
2023-06-13 19:45:34 -0400

    revert

---
+++ public/server/controllers/userController.js
@@ -0,0 +1,44 @@
+const userController = {};
+import userModel from '../models/UserModel.js';
+import { genSalt, hash, compare } from 'bcrypt';
+
+userController.createUser = function (req, res, next) {
+	try {
+		const { username, password } = req.body;
+		genSalt(10, (err, salt) => {
+			hash(password, salt, async (err, hash) => {
+				const newUser = await userModel.create({ username, password: hash });
+				res.locals.newUser = username;
+				return next();
+			});
+		});
+	} catch (err) {
+		return next(err);
+	}
+};
+
+userController.verifyUser = async function (req, res, next) {
+	try {
+		const { username, password } = req.body;
+		console.log(username);
+		const user = await userModel.findOne({ username: username });
+		console.log(user);
+		if (user === null) {
+			return res.json({ verified: false });
+		}
+		compare(password, user.password, (err, result) => {
+			if (err) {
+				return next(err);
+			}
+			if (result) {
+				return next();
+			} else {
+				return res.send(JSON.stringify({ verified: false }));
+			}
+		});
+	} catch (err) {
+		return next(err);
+	}
+};
+
+export default userController;
