89aadf1ae81a1b42c3a83c04395f5e096e7ac0f9
atvanek <99850013+atvanek@users.noreply.github.com>
2023-06-14 09:27:22 -0400

    changed case of login page

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './src/components/App.jsx';
-import Login from './src/pages/login.jsx';
+import Login from './src/pages/Login.jsx';
 import SignUp from './src/pages/SignUp.jsx';
 import Room from './src/pages/Room.jsx';
 import { ContextProvider } from './src/context/context.jsx';
