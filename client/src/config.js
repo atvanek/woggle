060a64d5945e017040a2eb75a127116227faae81
atvanek <99850013+atvanek@users.noreply.github.com>
2023-06-14 10:33:43 -0400

    typo

--- client/src/config.js
+++ client/src/config.js
@@ -3,10 +3,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
 	DEV_PORT: 8080,
 	WS_BASE_URL: isProduction
-		? 'https://woggle.herokuapp.com/'
+		? 'https://woggle.herokuapp.com'
 		: 'http://localhost:8080',
 	API_BASE_URL: isProduction
-		? 'https://woggle.herokuapp.com/'
+		? 'https://woggle.herokuapp.com'
 		: 'http://localhost:8080/api',
 };
 
