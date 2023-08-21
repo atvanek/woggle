c302a3a9b521c27590bcc2d6d868c7f5a9136741
atvanek <99850013+atvanek@users.noreply.github.com>
2023-08-21 10:50:55 -0400

    deleted typo in config

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,10 +3,10 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
 	DEV_PORT: 8080,
 	WS_BASE_URL: isProduction
-		? 'https://https://woggle.vercel.app'
+		? 'https://woggle.vercel.app'
 		: 'http://localhost:3000',
 	API_BASE_URL: isProduction
-		? 'https://https://woggle.vercel.app'
+		? 'https://woggle.vercel.app'
 		: 'http://localhost:8080/api',
 };
 
