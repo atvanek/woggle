896767eaa6f8e278348bb6bdd4be83d8957b23c1
atvanek <99850013+atvanek@users.noreply.github.com>
2023-09-11 15:23:16 -0400

    Col on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,21 @@ export default function Home() {
 	return (
 		<>
 			<h2 className='text-4xl text-center my-5'>Woggle</h2>
-			<main className='flex w-full my-20'>
-				<section id='played-words' className='w-4/12'>
+			<main className='flex w-full my-20 max-md:flex-col sm:max-md:flex-wrap'>
+				<section
+					id='played-words'
+					className='w-4/12 min-w-min max-md:w-full'>
 					<PlayedWords />
 				</section>
-				<section id='board' className='flex flex-col items-center w-4/12'>
+				<section
+					id='board'
+					className='flex flex-col items-center w-4/12 min-w-min max-md:w-full'>
 					<Board />
 					<Buttons />
 					<Score />
 					<Alert />
 				</section>
-				<section className='flex flex-col w-4/12 items-center'>
+				<section className='flex flex-col w-4/12 items-center min-w-min max-md:w-full'>
 					<TimeToggle />
 				</section>
 			</main>
