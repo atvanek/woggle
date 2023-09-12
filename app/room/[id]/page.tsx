b77634f9005f3905d4133fb91456761037ddac2c
atvanek <99850013+atvanek@users.noreply.github.com>
2023-09-12 17:08:03 -0400

    websocket connected to server

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,11 +1,22 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import {io} from 'socket.io-client'
+import { io } from 'socket.io-client';
+import { useEffect } from 'react';
 
 function Page() {
 	const params = useParams();
 	const id = params.id as string;
+	const socket = io('http://localhost:8080/');
+	useEffect(() => {
+		socket.on('connect', () => {
+			console.log('connected')
+		});
+		return () => {
+			socket.removeAllListeners();
+			socket.disconnect();
+		};
+	}, [socket]);
 	return <p>You are in room {decodeURI(id)}</p>;
 }
 
