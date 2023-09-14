425995812f6dd496d41d208539135666fe7f2894
atvanek <99850013+atvanek@users.noreply.github.com>
2023-09-13 20:23:48 -0400

    fixes multiple ws connections

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,17 +1,17 @@
 import RoomContainer from '@/components/containers/RoomContainer';
 
-export async function generateStaticParams() {
-	const params: { id: string }[] = [];
+// export async function generateStaticParams() {
+// 	const params: { id: string }[] = [];
 
-	const data = await fetch(
-		'https://cdn.jsdelivr.net/npm/@emoji-mart/data'
-	).then((res) => res.json());
+// 	const data = await fetch(
+// 		'https://cdn.jsdelivr.net/npm/@emoji-mart/data'
+// 	).then((res) => res.json());
 
-	for (const key in await data.emojis) {
-		params.push({ id: key });
-	}
-	return params;
-}
+// 	for (const key in await data.emojis) {
+// 		params.push({ id: key });
+// 	}
+// 	return params;
+// }
 
 function Page({ params }: { params: { id: string } }) {
 	const { id } = params;
--- hooks/useSocketConnect.tsx
+++ hooks/useSocketConnect.tsx
@@ -1,44 +1,49 @@
 'use client';
 
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 import { useEffect, useState } from 'react';
 
 function useSocketConnect(id: string) {
-	const [error, setError] = useState(false);
-	const [loading, setLoading] = useState(false);
-	const [message, setMessage] = useState('');
-
 	const WS_SERVER_URL = (
 		process.env.NODE_ENV === 'production'
 			? process.env.NEXT_PUBLIC_PROD_WS_SERVER
 			: process.env.NEXT_PUBLIC_DEV_WS_SERVER
 	) as string;
 
-	const socket = io(WS_SERVER_URL);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [message, setMessage] = useState('Connecting to your room...');
+	const [socket, setSocket] = useState<Socket | null>(null);
+
 	useEffect(() => {
 		setLoading(true);
-		setMessage('Connecting to your room...');
 		return () => {
 			setLoading(false);
 		};
 	}, []);
 
 	useEffect(() => {
-		socket.on('connect', () => {
-			console.log('connected');
-			setLoading(false);
-			setMessage(`You are in room ${id}`);
-		});
-		socket.io.on('error', () => {
-			setError(true);
-			setLoading(false);
-			setMessage('Error connecting to server. Please try again later.');
-		});
+		if (!socket) {
+			let newSocket = io(WS_SERVER_URL);
+
+			newSocket.on('connect', () => {
+				setLoading(false);
+				setMessage(`You are in room ${id}`);
+			});
+			newSocket.io.on('error', () => {
+				setError(true);
+				setLoading(false);
+				setMessage('Error connecting to server. Please try again later.');
+			});
+			setSocket(newSocket);
+		}
 		return () => {
-			socket.removeAllListeners();
-			socket.disconnect();
+			if (socket) {
+				socket.removeAllListeners();
+				socket.disconnect();
+			}
 		};
-	}, [socket, id]);
+	}, [socket, id, WS_SERVER_URL]);
 	return { error, loading, message };
 }
 export default useSocketConnect;
