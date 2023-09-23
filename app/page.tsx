b79f86e26e13fb9166dd42ba41fe4c1102c69f05
atvanek <99850013+atvanek@users.noreply.github.com>
2023-09-23 11:59:16 -0500

    corrected server url

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,9 @@ import RoomPicker from '@/components/containers/RoomPicker';
 import PlayedWords from '@/components/containers/PlayedWords';
 import Board from '@/components/containers/Board';
 import TimeToggle from '@/components/views/TimeToggle';
+import SERVER_URL from '@/utils/serverURL';
 
 async function getLetters() {
-	const SERVER_URL =
-	process.env.NEXT_PUBLIC_NODE_ENV === 'development'
-		? (process.env.NEXT_PUBLIC_DEV_WS_SERVER as string)
-		: (process.env.NEXT_PUBLIC_PROD_WS_SERVER as string);
 	const res: Response = await fetch(`${SERVER_URL}/letters`);
 	const letters: Promise<Letters> = await res.json();
 	return letters;
